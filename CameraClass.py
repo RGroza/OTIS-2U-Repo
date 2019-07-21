2cca3213ae700b357504daf8dfbd64ab21346d3c
RGroza <robertogroza@gmail.com>
2019-07-21 19:53:38 -0400

    changed camera class and added images file

--- CameraClass.py
+++ CameraClass.py
@@ -5,10 +5,10 @@ class CameraClass:
   #option to use jpeg, png, bmp, gif formats
   def takePic(self, name, imgFormat):
     with picamera.PiCamera() as camera:
-      camera.resolution = (1280,720)
-      camera.capture("/home/pi/Documents/CubeSats/images/Test_Full/" + name + "." + imgFormat, format=imgFormat)
+      #camera.resolution = (1280,720)
+      #camera.capture("/home/pi/Documents/CubeSats/images/Test_Full/" + name + "." + imgFormat, format=imgFormat)
       camera.resolution = (128,128)
       camera.capture("/home/pi/Documents/CubeSats/images/Test_Half/" + name + "." + imgFormat, format=imgFormat)
-      
+
   def resizePic(self, name):
     
