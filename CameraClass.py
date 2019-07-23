87030233d883e3cb9165801e99cab9534a3c6de1
Roberto Groza <robertogroza@gmail.com>
2019-07-23 19:57:20 -0400

    Update CameraClass.py

--- CameraClass.py
+++ CameraClass.py
@@ -1,15 +1,23 @@
 #hi
 import picamera
+from time import sleep
 
 class CameraClass:
   #option to use jpeg, png, bmp, gif formats
   def takePic(self, name):
     with picamera.PiCamera() as camera:
-      camera.resolution = (1280,720)
-      camera.capture("/home/pi/Documents/CubeSats/images/Test_Full/" + str(name) + ".png")
+      camera.iso = 100
+      sleep(2)
+      camera.shutter_speed = camera.exposure_speed
+      camera.exposure_mode = 'off'
+      g = camera.awb_gains
+      camera.awb_mode = 'off'
+      camera.awb_gains = g
+      #camera.resolution = (1280,720)
+      #camera.capture("/home/pi/CubeSats/" + str(name) + ".png")
       camera.resolution = (128,128)
-      camera.capture("/home/pi/Documents/CubeSats/images/Test_Half/" + str(name) + ".png")
-    return "/home/pi/Documents/CubeSats/images/Test_Half/" + str(name) + ".png"
+      camera.capture("/home/pi/CubeSats/" + str(name) + ".png")
+    return "/home/pi/CubeSats/" + str(name) + ".png"
       
   #def resizePic(self, name):
-    
+   
