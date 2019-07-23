934335946d2d4127ce20c97a3b306d08bc53cc7e
Roberto Groza <robertogroza@gmail.com>
2019-07-23 08:36:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ import ProcessClass
 import time
 import xbee_driver as xbee
 import power_system
+#import RPi.GPIO as GPIO
 
 GPS = GPSClass.GPSClass(-1,-1,0)
 camera = CameraClass.CameraClass()
-process = ProcessClass.ProcessClass()
 inc = 0
 power = power_system.power_system()
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(#insert num, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 #Every ~2 minutes, reset odometer
 while True: #Insert onAB() from IMU class
+   #if GPIO.input(num):
+    #   inc += 1
     name = camera.takePic(inc)
     if process.middlePix(name):
         if process.midRowPix(name):
-            list = process.picIdentify(name)
+            #print inc
+            list = processClass.picIdentify(name)
             GPS.getGPS()
             print (list)
             xbee.send(name)
@@ -26,5 +31,4 @@ while True: #Insert onAB() from IMU class
 
     print ("Battery charge: " + power.getBattPercent())
     print ("Voltage: " + power.getBattV()) #unsure if this gives voltage of solar panels
-    inc += 1
     time.sleep(0.5)
