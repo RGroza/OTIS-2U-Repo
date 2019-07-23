2ef0345b24cba1589f904a9625035cf7cadcc651
Roberto Groza <robertogroza@gmail.com>
2019-07-22 22:28:04 -0400

    xbee update

--- xmodem_recv.py
+++ xmodem_recv.py
@@ -2,5 +2,4 @@ from xbee_driver import xbee_driver as XBee
 
 RPi_sender = XBee()
 
-imgFile = open('/home/pi/Documents/OTIS-2U-Repo/images/test.png', 'wb')
-RPi_sender.recv(imgFile)
+RPi_sender.recv('/home/pi/Documents/OTIS-2U-Repo/images/test.png')
--- xmodem_send.py
+++ xmodem_send.py
@@ -2,5 +2,4 @@ from xbee_driver import xbee_driver as XBee
 
 RPi_sender = XBee()
 
-imgFile = open('/home/pi/Documents/OTIS-2U-Repo/images/test.png', 'rb')
-RPi_sender.send(imgFile)
+RPi_sender.send('/home/pi/Documents/OTIS-2U-Repo/images/test.png')
