4035465b32ae2b5f14be45e393f2cf7c453f431e
Roberto Groza <robertogroza@gmail.com>
2019-07-28 15:11:00 -0400

    Update XBee_sat.py

--- XBee_sat.py
+++ XBee_sat.py
@@ -2,8 +2,8 @@ from XBee_class import XBee
 from PIL import Image
 from time import sleep
 
-def convert_to_JPG(file): # filepath as string
-    im = Image.open(file)
+def convert_to_JPG(mydir, file): # filepath as string
+    im = Image.open(mydir + file)
     bg = Image.new('RGB', im.size, (255,255,255))
     bg.paste(im, (0,0), im)
     bg.save(file[0:file.index('.')] + '.jpg', quality=50)
@@ -12,7 +12,7 @@ def convert_to_JPG(file): # filepath as string
 def sendFileBatch(mydir, filesNum, prevFileType='.png', convertedFileType='.jpg'): # n -> number of images as int, fileType as string '.jpg'
     for n in range(filesNum):
         print("Converting image " + str(n))
-        convert_to_JPG(str(n) + prevFileType)
+        convert_to_JPG(mydir, str(n) + prevFileType)
         print("Image converted, sending image")
         satellite.send_file(mydir + str(currentFile) + convertedFileType)
         print("Image sent!")
