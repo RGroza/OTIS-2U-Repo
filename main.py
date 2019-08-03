8c8b8e6de94b6c5fdeb438b5bc432c9484235ba5
Chacko <mathaic@bxscience.edu>
2019-08-03 17:27:20 -0400

    updated main

--- main.py
+++ main.py
@@ -28,10 +28,15 @@ increment = 1
 roberto = 0
 while True: #imu.getDirection() == "N":
    telemetry = open("/home/pi/Documents/OTIS-2U-Repo/images/telemetry.txt", "w")
-   while count < 5 and roberto <= 3: #imu.getDirection() == "N":
+   while count < 7 and roberto <= 3: #imu.getDirection() == "N":
       #Picamera takes an image
       #telemetry = open("/home/pi/Documents/OTIS-2U-Repo/images/telemetry" + str(count) + ".txt", "a")
       name = camera.takePic(inc)
+      if GPIO.input(5) is not lastValue:
+          lastValue = GPIO.input(5)
+          counter+=1
+                  #prints out the amount of distance OTIS has travelled by calculating the circumference of the rotations
+          distance = str((counter * (0.055 * math.pi)))
       #Determines if the image encompasses the pan
       #if process.middlePix(name) and process.midRowPix(name):
       list = process.picIdentify(name)
@@ -39,6 +44,7 @@ while True: #imu.getDirection() == "N":
                #GPS.getGPS()
                #Prints out oxidation rate
       telemetry.write(str(list) + "\n")
+      telemetry.write(str(distance) + "\n")
                #Returns if dispersants would be effective
                #print process.determineEffectiveness(list)
                #Sends image to ground station
@@ -65,25 +71,19 @@ while True: #imu.getDirection() == "N":
       #telemetry.write(sol_voltage_2)
       #telemetry.write(sol_voltage_3)
       #telemetry.write(sol_voltage_4)
-      if GPIO.input(5) is not lastValue:
-          lastValue = GPIO.input(5)
-          counter+=1
-                  #prints out the amount of distance OTIS has travelled by calculating the circumference of the rotations
-          distance = str((counter * (0.055 * math.pi)))
           #telemetry.write(str(distance))
                #telemetry.readlines()
       count += 1
       #Deliveers telemetry data
 
       inc+=1
-      time.sleep(0.5)
+      time.sleep(0.25)
    telemetry.write(str(batt_charge) + "\n")
    telemetry.write(str(temp))
    telemetry.write(str(sol_voltage_1) + "\n")
    telemetry.write(str(sol_voltage_2) + "\n")
    telemetry.write(str(sol_voltage_3) + "\n")
    telemetry.write(str(sol_voltage_4) + "\n")
-   telemetry.write(str(distance) + "\n")
    telemetry.close()
    XBee_sat.send_files()
    increment += 1
